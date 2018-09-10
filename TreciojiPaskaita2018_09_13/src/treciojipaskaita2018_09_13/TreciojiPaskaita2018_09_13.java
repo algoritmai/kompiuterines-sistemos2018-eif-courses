2b35fe35bd0e03dfe8a9ed02d3d2954940557303
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2018-09-10 19:04:04 +0300

    Paskaitų medžiaga

---
+++ TreciojiPaskaita2018_09_13/src/treciojipaskaita2018_09_13/TreciojiPaskaita2018_09_13.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package treciojipaskaita2018_09_13;
+
+/**
+ *
+ * @author Marius
+ */
+public class TreciojiPaskaita2018_09_13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
